37c6168951df6d60fcb304e3c23a3185bbfe9d13
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2023-02-06 09:38:27 +0100

    Modify example for debugging winit received characters

--- examples/input/keyboard_input_events.rs
+++ examples/input/keyboard_input_events.rs
@@ -1,17 +1,37 @@
 //! Prints out all keyboard events.
 
-use bevy::{input::keyboard::KeyboardInput, prelude::*};
+use bevy::{input::keyboard::KeyboardInput, log::LogPlugin, prelude::*};
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    window: WindowDescriptor {
+                        prevent_default_event_handling: false,
+                        ..default()
+                    },
+                    ..default()
+                })
+                .set(LogPlugin {
+                    filter: "debug,wgpu_core=warn,wgpu_hal=warn,naga=info,winit=trace".into(),
+                    level: bevy::log::Level::DEBUG,
+                }),
+        )
         .add_system(print_keyboard_event_system)
         .run();
 }
 
 /// This system prints out all keyboard events as they come in
-fn print_keyboard_event_system(mut keyboard_input_events: EventReader<KeyboardInput>) {
+fn print_keyboard_event_system(
+    mut keyboard_input_events: EventReader<KeyboardInput>,
+    mut received_character_events: EventReader<ReceivedCharacter>,
+) {
     for event in keyboard_input_events.iter() {
+        info!("{:?}", event);
+    }
+
+    for event in received_character_events.iter() {
         info!("{:?}", event);
     }
 }
